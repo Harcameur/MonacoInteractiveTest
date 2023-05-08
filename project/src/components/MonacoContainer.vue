071302860a10336a9345fb949edb87c517adb1a5
Harcameur <trawn2511@gmail.com>
2023-05-08 18:22:39 +0100

    first commit

---
+++ project/src/components/MonacoContainer.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import * as monaco from "monaco-editor";
+import { onMounted } from "vue";
+const props = defineProps(['content'])
+onMounted(() => {
+    const comp = document.getElementById("EditorContainer");
+    if(comp){
+        console.log("init");
+        const editor = monaco.editor.create(comp, {
+            value: props.content? props.content : "",
+            theme: "vs-dark",
+            glyphMargin: true,
+    });
+}
+})
+
+
+
+</script>
+
+<template>
+        <h1>Editor Renderning</h1><br>
+        <div id="EditorContainer">
+            <!-- editor div -->
+        </div>
+</template>
+
+<style scoped>
+#EditorContainer{
+    width: 500px;
+    height: 300px;
+}
+</style>
---
+++ project/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import MonacoContainer from '../components/MonacoContainer.vue';
+
+</script>
+
+<template>
+  <main>
+    <MonacoContainer/>
+  </main>
+</template>
