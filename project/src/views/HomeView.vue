5d5520086449659c83effd09a0361ac9254634d2
Harcameur <trawn2511@gmail.com>
2023-05-10 08:37:30 +0100

    add deployment

--- project/src/views/HomeView.vue
+++ project/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import MonacoContainer from '../components/MonacoContainer.vue';
-import SemanticInputContainer from '../components/SemanticINputContainer.vue';
+import SemanticInputContainer from '../components/SemanticInputContainer.vue';
 import SemanticSequencePreview from '../components/SemanticSequencesPreview.vue';
 
 </script>
